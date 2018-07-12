69a6ec3f6622377b3d9a7edbf8becde4002765f9
CMO <catherine@msdlab.com>
2018-07-12 13:26:01 -0400

    zoo new bug

--- system/assets/themes/fionasworld/lib/js/homepage-jquery.js
+++ system/assets/themes/fionasworld/lib/js/homepage-jquery.js
@@ -1,2 +1,8 @@
 jQuery(document).ready(function($) {
+    $('.section-zoo-news .section-title h3').html(function(){
+        var str = $(this).html().trim();
+        var h = str.split(" ");
+        console.log(h);
+        return '<span class="lg">' + h[0] + '</span><span class="sm">' + h[1] + '</span>';
+    });
 });
