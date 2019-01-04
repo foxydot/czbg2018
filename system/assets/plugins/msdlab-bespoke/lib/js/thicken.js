8349a72bb48f1366a3f6ae59109ca569cac11507
CMO <catherine@msdlab.com>
2019-01-04 14:42:33 -0500

    fix thicken

--- system/assets/plugins/msdlab-bespoke/lib/js/thicken.js
+++ system/assets/plugins/msdlab-bespoke/lib/js/thicken.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($) {
-    var link = $('img').parent('a');
-    var href = link.attr('href');
-    if(href.indexOf('.png') !== -1 || href.indexOf('.jpg') !== -1 || href.indexOf('.jpeg') !== -1 || href.indexOf('.svg') !== -1){
-        link.attr("data-fancybox","gallery");
-    }
+    $('a > img').each(function(){
+        var link = $(this).parent('a');
+        var href = link.attr('href');
+        //console.log('href: ' + href);
+        if(href.indexOf('.png') > 1 || href.indexOf('.jpg') > 1 || href.indexOf('.jpeg') > 1 || href.indexOf('.svg') > 1){
+            link.attr("data-fancybox","gallery");
+        }
+    });
 });
